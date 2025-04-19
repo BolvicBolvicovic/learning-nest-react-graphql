b6a7567f3c1686855ef5f7ce422f1cf01e8077e6
Victor Cornille <victor.cornille@gmail.com>
2025-04-19 10:00:15 +0200

    Testing lucid react

--- lesson01/frontend/src/pages/Home.tsx
+++ lesson01/frontend/src/pages/Home.tsx
@@ -1,12 +1,16 @@
 import { PetsForm } from "@/components/petsForm";
 import { PetsList } from "@/components/petsList";
 import { UsersList } from "@/components/usersList";
+import { Origami } from "lucide-react";
 
 export default function Home() {
 
     return (
       <div className="flex flex-col items-center w-screen p-4">
-        <h1>Pet Shop</h1>
+        <h1 className="flex flex-row items-center justify-center">
+          <Origami size={48} />
+          Pet Shop
+        </h1>
         <div className="flex flex-raw max-row-2 items-center justify-center min-h-screen w-screen space-x-4">
           <UsersList />
         </div>
